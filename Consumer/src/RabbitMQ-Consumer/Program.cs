dcfdb0d8c28b6916931772d43c59603cbe2d335b
Icaro Milet <icaro.soares@gipsyy.com.br>
2023-10-25 10:28:42 -0300

    feat: Publisher and Consumer

---
+++ Consumer/src/RabbitMQ-Consumer/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+
+var connection = factory.CreateConnection();
+
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(
+        queue: "Teste",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine($" [X] Recebida: {message}");
+    };
+
+    channel.BasicConsume(
+        queue: "Teste",
+        autoAck: true,
+        consumer: consumer);
+
+    Console.ReadLine();
+}
---
+++ Publisher/src/RabbitMQ-Publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+
+var connection = factory.CreateConnection();
+
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(
+        queue: "Teste",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null);
+
+    string message = "Bem-vindo ao RabbitMQ";
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(
+        exchange: "",
+        routingKey: "Teste",
+        basicProperties: null,
+        body: body);
+
+    Console.WriteLine($" [X] Enviada: {message}");
+}
